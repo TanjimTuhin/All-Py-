1b4a63e0a3e2ddb2d16dbd4e33d17dad8e614cdc
TanjimTuhin <tanjimtuhin06@gmail.com>
2024-11-02 23:26:35 +0600

    5

--- py OOp/L5 Instance Variable & Method.py
+++ py OOp/L5 Instance Variable & Method.py
@@ -4,15 +4,22 @@ class Student:
         self.name = name   #instance variable
         self.id = id       #instance variable
         #print('a student object created')
-
+    def details(self):    #instance method  
+        print("Name:",self.name,"ID:",self.id)
+        
+        
 #creating object ==============================
 s1 = Student('John',1)
 s2 = Student('Alice',2)
-print(s1.name,s1.id)
+s1.id=24
+# print(s1.name,s1.id)
+s1.details()
+s2.details()
 
-s1.id=33 #variable update 
-print(s1.name,s1.id)
-print(s2.name,s2.id)
+
+# s1.id=33 #variable update 
+# print(s1.name,s1.id)
+# print(s2.name,s2.id)
 
 # print("s1", s1)
 # print("s2", s2)
