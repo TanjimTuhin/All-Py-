33109e65169ccfbf20344884e2781a26287f0b34
TanjimTuhin <tanjimtuhin06@gmail.com>
2024-11-02 23:12:36 +0600

    5

---
+++ py OOp/L5 Instance Variable & Method.py
@@ -0,0 +1,18 @@
+"""Class Object Constructor attributes methods"""
+class Student:
+    def __init__(self,name,id): #name,id ->attribute
+        self.name = name   #instance variable
+        self.id = id       #instance variable
+        #print('a student object created')
+
+#creating object ==============================
+s1 = Student('John',1)
+s2 = Student('Alice',2)
+print(s1.name,s1.id)
+
+s1.id=33 #variable update 
+print(s1.name,s1.id)
+print(s2.name,s2.id)
+
+# print("s1", s1)
+# print("s2", s2)
