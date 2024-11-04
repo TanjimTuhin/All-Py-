7ff1b2be03883cc9dd526e80d40627b1dbf8a941
TanjimTuhin <tanjimtuhin06@gmail.com>
2024-11-04 19:22:10 +0600

    13

--- py OOp/L13 Method Overloading part 2.py
+++ py OOp/L13 Method Overloading part 2.py
@@ -1,21 +1,38 @@
-#Method Overloading= multiple method with same name in a class with diferrent variable
-class my_calculator:
-    def product(self, num1, num2=None, num3=None):
-        if num1!=None and num2 !=None and num3!=None:
-            print(num1 * num2* num3)
-        elif num1!=None and num2 !=None:
-            print(num1 * num2)
-        else:
-            print(num1)
+#Method Overloading = multiple method with same name in a class with diferrent variable
 
-    def product(self, *nums):
-        sum= 1
-        for x in nums:
-            sum*= x
-        print(sum)
+from multipledispatch import dispatch
+class my_calculator:
+    @dispatch(int)
+    def product(self, num1):
+        print( num1 )
         
+    @dispatch(int, int)
+    def product(self, num1, num2):
+        print( num1 * num2 )
+    
+    @dispatch(int, int, int)
+    def product(self, num1, num2, num3):
+        print(num1 * num2* num3)
+
+    @dispatch(str, str)
+    def product(self, num1, num2):
+        print( int(num1) * int(num2) )
+        
+    @dispatch(float, float, float)
+    def product(self, num1, num2, num3):
+        print(num1 * num2* num3)
+        
+    @dispatch(str, int)
+    def product(self, num1, num2):
+        print("eita string ar integer value")
+        print( int(num1) *(num2) )
+    
+            
 #creating object=============
 c1= my_calculator()
 c1.product(4)
 c1.product(4,5)
 c1.product(4,5,6)
+c1.product("4","5") 
+c1.product(4.0,3.0,6.3)
+c1.product("4",5)
