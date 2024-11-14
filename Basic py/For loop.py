4cb9b48b289b761b736aaab1fb0c1b2723aa8a33
TanjimTuhin <tanjimtuhin06@gmail.com>
2024-11-14 15:43:14 +0600

    2

--- Basic py/For loop.py
+++ Basic py/For loop.py
@@ -1,1 +1,69 @@
-#
+# """ print-> a[1,2,3,4,5] ==============================="""
+
+# a=[1,2,3,4,5]
+# #WHILE ------------------------
+# # i=0
+# # while i<len(a):
+# #     print(a[i])
+# #     i+=1
+# #FOR --------------------------
+# for i in a:
+#     print(i)
+
+# """ Sum of List -> [22, 44, 33, 51, 64]================="""
+
+# a= [22, 44, 33, 51, 64 ]
+# sum=0
+# for i in a:
+#     sum+=i
+# print(sum)
+
+# """find the largest number ============="""
+# a=[22, 44, 33, 51, 64]
+
+# largest=-99
+# for i in a:
+#     if i>largest:
+#         largest=i
+#     else:
+#         largest=largest
+# print(largest)
+
+"""For loop ==================================================
+1. Break keyword
+2. Continue keyword
+3. range() keyword
+4. Nested for loop
+"""
+
+# flowers=["Lily","Mily","Rose","Cherry","Orchid"]
+# for x in flowers:
+#     if x=="Rose":
+#         break
+#     print(x) 
+
+# flowers=["Lily","Mily","Rose","Cherry","Orchid"]
+# for x in flowers:
+#     if x=="Rose":
+#         continue
+#     print(x) 
+
+#range() function----------------------------
+#range(start, stop, step)
+# flowers=["Lily","Mily","Rose","Cherry","Orchid"]
+# for x in range(3,10,2):
+#     print(x)
+
+# for x in range(30,10,-2):
+#     print(x)
+
+colors=["White","Blue","Gray","Black"]
+cars=["BMW","Audi","Toyota","Koinigsegg","Ferrari"]
+for x in colors:
+    for y in cars:
+        print(x,y)
+
+
+
+
+
